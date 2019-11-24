1b77c477b88e1a71dfa42f03b9507dd580425b32
Alwani Akhbar <57653617+Aiwani@users.noreply.github.com>
2019-11-24 12:06:41 +0800

    Update Leaderboard.php

--- Leaderboard.php
+++ Leaderboard.php
@@ -4,7 +4,7 @@ $conn = new mysqli($HostName, $HostUser, $HostPass, $DatabaseName);   // creatin
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);   // displays error message when connection is failed 
 } 
-$sql = "SELECT * FROM user ORDER BY total_trim_point DESC";   // select total trim points to be displayed in leaderboard ranking 
+$sql = "SELECT * FROM user ORDER BY total_trim_point DESC";   // select total trim points to be displayed in leaderboard ranking
 $result = $conn->query($sql);
 if ($result->num_rows >0) {
 	while($row[] = $result->fetch_assoc()) {    
