c7030d14d63eea353bfa5eedfe01cd8f13292d30
Alwani Akhbar <57653617+Aiwani@users.noreply.github.com>
2019-11-24 11:58:30 +0800

    Update Leaderboard.js

--- Leaderboard.js
+++ Leaderboard.js
@@ -12,18 +12,18 @@ export default class Rewards extends Component {
 
     async componentDidMount() {
         try {
-            const response = await fetch('http://192.168.1.193/2203scripts/Leaderboard.php');
+            const response = await fetch('http://192.168.1.193/2203scripts/Leaderboard.php'); // connection to the DB, phpMyAdmin
             const responseJson = await response.json();
-            this.setState({
+            this.setState({                                                      // set current data in DB to update the ones in Leaderboard page
                 dataSource: responseJson,
             });
         }
         catch (error) {
-            console.error(error);
+            console.error(error); // no response, error handling
         }
     };
 
-    renderHeader() {
+    renderHeader() {      // alignments & style for Leaderboard Header 
         return (
         <View style={{ backgroundColor: '#119abf', padding: 15, paddingTop: 35, alignItems: 'center' }}>
             <Text style={{ fontSize: 25, color: 'white', }}>Points LeaderBoard</Text>
@@ -33,13 +33,13 @@ export default class Rewards extends Component {
 
     render() {
         return (
-        <View style={{ flex: 1, backgroundColor: 'white' }}>
+        <View style={{ flex: 1, backgroundColor: 'white' }}>    // style for Leaderboard "ranking"
             {this.renderHeader()}
             <Leaderboard 
-            data={this.state.dataSource} 
-            sortBy='total_trim_point' 
-            labelBy='username' 
-            icon="pic" />
+            data={this.state.dataSource}   // fetch data from phpMyAdmin
+            sortBy='total_trim_point'     // sort users according to accumulated points 
+            labelBy='username'           // display username
+            icon="pic" />               // display dp of user 
         </View>
     )};
 }
