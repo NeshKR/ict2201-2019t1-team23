65c7e152f2df54d771fbba1e3f90308abd3f2c7c
sunzhiying1998 <47717308+sunzhiying1998@users.noreply.github.com>
2019-11-29 14:42:09 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import Login from './Src/Login'
+import Old_Homepage from './Src/Old_Homepage'
 import Feedback from './Src/Feedback'
 import Challenge from './Src/Challenge'
 import ExampleMap from './Src/Map'
@@ -6,14 +7,20 @@ import Homepage from './Src/Homepage'
 import Profile from './Src/Profile'
 import Custom_Leaderboard from './Src/Leaderboard'
 import Reward from './Src/Reward'
+import TestProfile from './Src/testProfile'
+import Cba from './Src/Get_Current_Location_Map'
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
 const RootStack = createStackNavigator ({
+    //Challenge_Screen : {screen: Challenge},
     Login_Screen : {screen: Login},
     Map_Screen : {screen: ExampleMap},
+    Cba_Screen : {screen: Cba},
+    Test_Profile_Screen : {screen: TestProfile},
     Homepage_Screen : {screen: Homepage},
     Feedback_Screen : {screen: Feedback},
+    Old_Homepage_Screen : {screen: Old_Homepage},
     Challenge_Screen : {screen: Challenge},
     Profile_Screen : {screen: Profile},
     Leaderboard_Screen : {screen: Custom_Leaderboard},
